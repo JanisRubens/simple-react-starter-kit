9d1f1f982c2fbc78a697715dce0732a6523173f6
unknown <janis.rubens@LVADTP0259.dir.svc.accenture.com>
2016-07-12 14:53:17 +0300

    auth endpoints with passport implemented

--- server.js
+++ server.js
@@ -2,21 +2,41 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var router = express.Router();
 var routes = require('./server/routes/main');
+var session = require('express-session');
+var passport = require('passport');
+var initPassport = require('./server/passport-init');
 
 var PORT = process.env.PORT || 3000;
 
 var ENV = process.env.NODE_ENV;
 var resFile = (JSON.stringify(ENV).indexOf('production') > -1) ? 'index' : 'index-dev';
 
+app.use(session({
+	secret: 'wilde cracken appears', //this should bee hidden from repo 
+	name: 'session-token',//cookie_name,
+	//store: sessionStore, // connect-mongo session store
+	proxy: true,
+	resave: true,
+	saveUninitialized: true
+}));
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // Used for production build
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public'), {index: resFile + '.html'}))
 
-routes(app);
+app.use(passport.initialize());
+app.use(passport.session());
+
+routes(app, passport);
+
+initPassport(passport);
 
 app.all('/*', function(req, res) {
 	res.sendFile(path.join(__dirname, 'public/' + resFile + '.html'));
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,6 +1,30 @@
 // Require all routes that app will use;
 var apiRoutes = require('./api/routes');
+//var passport = require('passport');
+//var initPassport = require('../passport-init');
+var authRoutes = require('./auth/routes');
 
-module.exports = function routes(app) {
-    app.use('/api', apiRoutes);
+//Used for routes that must be authenticated.
+function isAuthenticated (req, res, next) {
+	// if user is authenticated in the session, call the next() to call the next request handler 
+	// Passport adds this method to request object. A middleware is allowed to add properties to
+	// request and response objects
+
+	//allow all get request methods
+	if(req.method === "GET"){
+		return next();
+	}
+	if (req.isAuthenticated()){
+		return next();
+	}
+
+	// if the user is not authenticated then redirect him to the login page
+	return res.redirect('/login');
+};
+
+module.exports = function routes(app, passport) {
+
+	app.use('/api', isAuthenticated);
+	app.use('/api', apiRoutes);
+	app.use('/auth', authRoutes(passport));
 };
